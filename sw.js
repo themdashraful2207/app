eba075841fb7ff66d5944e9b54c82bb207b61a66
themdashraful2207 <41838805+themdashraful2207@users.noreply.github.com>
2025-04-11 06:18:43 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/civilapp/menu.html',
-                '/civilapp/proxy.html',
-                '/civilapp/manifest.json',
-                '/civilapp/iconr.png',
-                '/civilapp/credentials.json',
-                '/civilapp/home.html' // Add if home.html exists
+                '/app/menu.html',
+                '/app/proxy.html',
+                '/app/manifest.json',
+                '/app/iconr.png',
+                '/app/credentials.json',
+                '/app/home.html' // Add if home.html exists
             ]);
         })
     );
